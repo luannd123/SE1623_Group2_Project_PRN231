2c2407f497d958a4d308875311b0551ad182e1c5
NDL <ngoluan3752@gmail.com>
2023-11-08 09:06:18 +0700

    update 2

---
+++ Client/Controllers/OrderController.cs
@@ -0,0 +1,85 @@
+﻿using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace Client.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly HttpClient client;
+        private string url = "";
+        public OrderController()
+        {
+            client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+        }
+
+        public async Task<ActionResult> IndexAsync()
+        {
+            if (HttpContext.Session.GetString("admin") is null) return Redirect("~/Auth/Login");
+            url = "https://localhost:7200/api/Order/GetOrders";
+            HttpResponseMessage response = await client.GetAsync(url);
+            string json = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            List<Order>? orderDTO = JsonSerializer.Deserialize<List<Order>>(json, options);
+            return View(orderDTO);
+        }
+        public async Task<ActionResult> DetailsAsync(int id)
+        {
+            url = "https://localhost:7200/api/OrderDetail/GetOrderDetails";
+            HttpResponseMessage response = await client.GetAsync(url + "/" + id);
+            string json = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            List<OrderDetail>? orderDetailDTO = JsonSerializer.Deserialize<List<OrderDetail>>(json, options);
+            return View(orderDetailDTO);
+        }
+
+        public async Task<ActionResult> Update(int id)
+        {
+            if (HttpContext.Session.GetString("admin") is null) return Redirect("~/Auth/Login");
+            url = "https://localhost:7200/api/Order/UpdateOrder";
+            HttpResponseMessage response = await client.GetAsync(url + "/" + id);
+            string json = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            Order? orderDTO = JsonSerializer.Deserialize<Order>(json, options);
+            return View(orderDTO);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> UpdateAsync(Order orderDTO)
+        {
+            if (HttpContext.Session.GetString("admin") is null) return Redirect("~/Authen/Login");
+            url = "https://localhost:7200/api/Order/UpdateOrder";
+            JsonContent content = JsonContent.Create(orderDTO);
+            HttpResponseMessage response = await client.PutAsync(url + "/" + orderDTO.OrderId, content);
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            if (HttpContext.Session.GetString("admin") is null) return Redirect("~/Auth/Login");
+            url = "https://localhost:7200/api/Order";
+            HttpResponseMessage response = await client.DeleteAsync(url + "/" + id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Client/Controllers/UserController.cs
@@ -0,0 +1,99 @@
+﻿using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Metrics;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace Client.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly HttpClient client = null;
+        private string url = "";
+        public UserController()
+        {
+            client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+        }
+        public async Task<IActionResult> Index()
+        {
+            if (HttpContext.Session.GetString("admin") is null) return Redirect("~/Authen/Login");
+            url = "https://localhost:7200/api/User/GetUser";
+            HttpResponseMessage response = await client.GetAsync(url);
+            string json = await response.Content.ReadAsStringAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            List<User>? users = JsonSerializer.Deserialize<List<User>>(json, options);
+            return View(users);
+        }
+        //https://localhost:7200/api/User/GetUser
+
+        public ActionResult Create()
+        {
+            if (HttpContext.Session.GetString("admin") is null) return Redirect("~/Auth/Login");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(User UserUpdateMemberDTO)
+        {
+            if (HttpContext.Session.GetString("admin") is null) return Redirect("~/Authen/Login");
+            url = "https://localhost:7200/api/User/AddNewUser";
+            JsonContent content = JsonContent.Create(UserUpdateMemberDTO);
+            HttpResponseMessage response = await client.PostAsync(url, content);
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public async Task<ActionResult> Update(int id)
+        {
+            if (HttpContext.Session.GetString("admin") is null) return Redirect("~/Auth/Login");
+            url = "https://localhost:7020/api/User/UpdateUser";
+            HttpResponseMessage response = await client.GetAsync(url + "/" + id);
+            string json = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            User? createUpdateMemberDTO = JsonSerializer.Deserialize<User>(json, options);
+            return View(createUpdateMemberDTO);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> UpdateAsync(User createUpdateMemberDTO)
+        {
+            if (HttpContext.Session.GetString("admin") is null) return Redirect("~/Auth/Login");
+            url = "https://localhost:7020/api/User/UpdateUser";
+            JsonContent content = JsonContent.Create(createUpdateMemberDTO);
+            HttpResponseMessage response = await client.PutAsync(url + "/" + createUpdateMemberDTO.UserId, content);
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            if (HttpContext.Session.GetString("admin") is null) return Redirect("~/Authen/Login");
+            url = "https://localhost:7200/api/User/DeleteUser";
+            HttpResponseMessage response = await client.DeleteAsync(url + "/" + id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
